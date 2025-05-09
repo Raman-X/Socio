602d3313fa5312c745807c94cc56ad1379218a45
raman <ramanmaharzan123@gmail.com>
2025-05-09 16:20:44 +0545

    made dropdown for toggling sort latest or oldest

--- client/src/scenes/widgets/SortToggle.jsx
+++ client/src/scenes/widgets/SortToggle.jsx
@@ -1,21 +1,21 @@
 // components/widgets/SortToggle.jsx
-import { ToggleButtonGroup, ToggleButton, Box } from "@mui/material";
+import { Box, FormControl, InputLabel, Select, MenuItem } from "@mui/material";
 
 const SortToggle = ({ sortOrder, setSortOrder }) => {
   return (
-    <Box sx={{ display: "flex", my: 2 }}>
-      <ToggleButtonGroup
-        value={sortOrder}
-        exclusive
-        onChange={(e, newOrder) => {
-          if (newOrder !== null) setSortOrder(newOrder);
-        }}
-        size="small"
-        color="primary"
-      >
-        <ToggleButton value="latest">Latest First</ToggleButton>
-        <ToggleButton value="oldest">Oldest First</ToggleButton>
-      </ToggleButtonGroup>
+    <Box sx={{ display: "flex", my: 2, minWidth: 120 }}>
+      <FormControl fullWidth size="small">
+        <InputLabel id="sort-order-label">Sort By</InputLabel>
+        <Select
+          labelId="sort-order-label"
+          value={sortOrder}
+          label="Sort By"
+          onChange={(e) => setSortOrder(e.target.value)}
+        >
+          <MenuItem value="latest">Latest First</MenuItem>
+          <MenuItem value="oldest">Oldest First</MenuItem>
+        </Select>
+      </FormControl>
     </Box>
   );
 };
